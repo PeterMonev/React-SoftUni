110b535dc25cf8d5fe3d7758809a110a75ec62b4
PeterMonev <monevpeter@gmail.com>
2024-02-14 14:45:24 +0200

    Create userService.js

---
+++ Form/form-workshop/client/src/services/userService.js
@@ -0,0 +1,29 @@
+const baseUrl = "http://localhost:3005/api/users";
+
+export const getAll = async () => {
+  const response = await fetch(baseUrl);
+  const result = await response.json();
+
+  return result.users;
+};
+
+export const getById = async (id) => {
+  const response = await fetch(`${baseUrl}/${id}`);
+  const result = await response.json();
+
+  return result.user;
+};
+
+export const create = async (userData) => {
+  const response = await fetch(baseUrl,{
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  }); 
+
+  const result = await response.json();
+
+  return result.user;
+}
