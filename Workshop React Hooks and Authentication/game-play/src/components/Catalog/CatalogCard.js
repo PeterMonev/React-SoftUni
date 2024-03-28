09e8a3f60a4b44630a4a5dd0f33fb3715e1f69cb
PeterMonev <monevpeter@gmail.com>
2024-03-28 09:01:44 +0200

    Create CatalogCard.js

---
+++ Workshop React Hooks and Authentication/game-play/src/components/Catalog/CatalogCard.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const CatalogCard = ({game}) => {
+    return (
+        <div className="allGames">
+        <div className="allGames-info">
+          <img src={game.imageUrl} />
+          <h6>{game.category}</h6>
+          <h2>{game.title}</h2>
+          <Link to={`/catalog/${game._id}`} className="details-button">
+            Details
+          </Link>
+          <Link style={{marginLeft: '130px'}} to={`/games/${game._id}/edit`} className="details-button">
+            Edit
+          </Link>
+        </div>
+      </div>
+    );
+};
+
+export default CatalogCard;
