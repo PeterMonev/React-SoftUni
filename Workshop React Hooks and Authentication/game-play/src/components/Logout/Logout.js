a92abe2c1b9d896635764443d021b0a6b2262c99
PeterMonev <monevpeter@gmail.com>
2024-03-30 10:20:48 +0200

    Create Logout.js

---
+++ Workshop React Hooks and Authentication/game-play/src/components/Logout/Logout.js
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useContext } from 'react';
+ 
+import * as authService from '../../services/authService';
+import { AuthContext } from '../../contexts/AuthContext';
+
+ const Logout = () => {
+    const navigate = useNavigate();
+    const { user, userLogout } = useContext(AuthContext);
+
+    useEffect(() => {
+        authService.logout(user.accessToken)
+          .then(() => {
+            userLogout();
+            navigate('/');
+          })
+          .catch(() => {
+            navigate('/');
+          })
+    })
+
+    return null;
+}
+
+export default Logout;
